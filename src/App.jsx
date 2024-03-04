094a5e3821c16377cc64ca7fef1864d37ca8d1d8
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-04 04:00:18 +0200

    add

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/1-Header/Header";
+import Hero from "./components/2-Hero/Hero";
+import Main from "./components/3-Main/Main";
+import Contact from "./components/4-Contact/Contact";
+import Footer from "./components/5-Footer/Footer";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Hero />
+      <Main />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/2-Hero/Hero.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./hero.css";
+
+export default function Hero() {
+  return <div>Hero</div>;
+}
