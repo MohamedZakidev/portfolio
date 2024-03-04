98986a7ad202269afbd5304fd348184eb8db12b5
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-04 18:09:29 +0200

    add

--- src/components/1-Header/Header.jsx
+++ src/components/1-Header/Header.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import './header.css'
 
 export default function Header() {
+    const [openNav, setOpenNav] = React.useState(false)
+
     return (
         <header className='flex'>
-            <button className='menu'>menu</button>
-            <nav>
+            <button
+                className='menu'
+                onClick={() => setOpenNav(!openNav)}
+            >
+                {!openNav ? <>&#9776;</> : <>&#10006; </>}
+            </button>
+            <div />
+            <nav className={openNav && "open-nav"}>
                 <ul className='flex'>
                     <li><a href="">About</a></li>
                     <li><a href="">Articles</a></li>
