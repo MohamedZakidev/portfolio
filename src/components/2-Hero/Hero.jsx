2cf62e4f224119993b1e7d11204e0d2d267e5be0
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-09 09:37:17 +0200

    jj

--- src/components/2-Hero/Hero.jsx
+++ src/components/2-Hero/Hero.jsx
@@ -11,7 +11,10 @@ export default function Hero() {
         </div>
         <h1 className="title">React Frontend Developer</h1>
         <p>Passionate about crafting exceptional user experiences. With a strong foundation in web technologies and a keen eye for design, I thrive on tackling complex challenges and turning them into simple, intuitive solutions.</p>
-        <div className="social-media"></div>
+        <div className="social-media">
+          <a href="https://github.com/MohamedZakidev" target="_blank"><span className="icon icon-github"></span></a>
+          <a href="https://www.linkedin.com/in/mohamed-zaki-55983b287/" target="_blank"><span className="icon icon-linkedin-square"></span></a>
+        </div>
       </div>
       <div className="right-section animation">animation</div>
     </section>
