7761d7a545be3b901a3d1b2b3539fa784cf797cf
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-09 08:32:56 +0200

    add

--- src/components/2-Hero/Hero.jsx
+++ src/components/2-Hero/Hero.jsx
@@ -9,8 +9,8 @@ export default function Hero() {
           <img className="avatar" src="/profile.png" alt="personal avatar" />
           <span className="icon-verified"></span>
         </div>
-        <h1>React Frontend Developer</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, reiciendis animi culpa, repellat corrupti eos modi quae voluptates ullam ad ab illum? Perspiciatis cupiditate ea possimus quidem nam ullam voluptas.</p>
+        <h1 className="title">React Frontend Developer</h1>
+        <p>Passionate about crafting exceptional user experiences. With a strong foundation in web technologies and a keen eye for design, I thrive on tackling complex challenges and turning them into simple, intuitive solutions.</p>
         <div className="social-media"></div>
       </div>
       <div className="right-section animation">animation</div>
