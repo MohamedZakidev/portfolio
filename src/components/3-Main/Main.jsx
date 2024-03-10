9f7bc413483b809b96d58e3fb26e364eb5f5ecd9
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-10 05:26:23 +0200

    add

--- src/components/3-Main/Main.jsx
+++ src/components/3-Main/Main.jsx
@@ -4,11 +4,6 @@ import './main.css'
 export default function Main() {
     return (
         <main className='flex '>
-            <section className="left-section flex">
-                <button className='active'>all projects</button>
-                <button>HTML & CSS & JavaScript</button>
-                <button>React</button>
-            </section>
             <section className="right-section">
                 sssssssssssssssssssssssssssss
             </section>
