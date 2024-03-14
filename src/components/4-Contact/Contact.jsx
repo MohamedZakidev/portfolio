fa2b2966e45fe030186ce5113839240594874dc0
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-14 23:21:58 +0200

    a

--- src/components/4-Contact/Contact.jsx
+++ src/components/4-Contact/Contact.jsx
@@ -1,5 +1,21 @@
 import "./contact.css";
 
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contact-us">
+      <h1 className="flex"><span className="icon icon-envelope"></span>Contact me</h1>
+      <p>Contact me for more information and Get notified when i puplish something new</p>
+      <div className="flex">
+        <form action="">
+          <label htmlFor="email">Email Adress:</label>
+          <input id="email" name="email" type="text" />
+
+          <label htmlFor="message">Your message:</label>
+          <textarea id="message" name="message" />
+          <button type="submit">Submit</button>
+        </form>
+        <div className="animation">animation</div>
+      </div>
+    </section>
+  );
 }
