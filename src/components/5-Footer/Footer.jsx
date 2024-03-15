87e31d2a5afffab5e313615f3bffcd23825d8839
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-16 01:43:10 +0200

    add

--- src/components/5-Footer/Footer.jsx
+++ src/components/5-Footer/Footer.jsx
@@ -2,5 +2,16 @@ import React from "react";
 import "./footer.css";
 
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="flex">
+      <ul>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+        <li><a href=""></a></li>
+      </ul>
+
+      <p>All rights reserved</p>
+    </footer>
+  );
 }
