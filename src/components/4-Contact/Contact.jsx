7fc18add93b7941ab711b5939a2f0e356c6e4ca5
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-03-15 11:54:53 +0200

    add

--- src/components/4-Contact/Contact.jsx
+++ src/components/4-Contact/Contact.jsx
@@ -6,12 +6,16 @@ export default function Contact() {
       <h1 className="flex"><span className="icon icon-envelope"></span>Contact me</h1>
       <p>Contact me for more information and Get notified when i puplish something new</p>
       <div className="flex">
-        <form action="">
-          <label htmlFor="email">Email Adress:</label>
-          <input id="email" name="email" type="text" />
+        <form className="flex">
+          <div className="flex">
+            <label htmlFor="email">Email Adress:</label>
+            <input id="email" name="email" type="text" />
+          </div>
 
-          <label htmlFor="message">Your message:</label>
-          <textarea id="message" name="message" />
+          <div className="flex">
+            <label htmlFor="message">Your message:</label>
+            <textarea id="message" name="message" />
+          </div>
           <button type="submit">Submit</button>
         </form>
         <div className="animation">animation</div>
